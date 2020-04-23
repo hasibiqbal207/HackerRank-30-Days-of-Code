bfba9d9f91bf025c265c9de304b76b5c85028d1f
hasibiqbal207 <hasibiqbal207@gmail.com>
2020-04-23 17:15:25 +0600

    Day1 solved

--- Day 1 - Data Types/sol1.py
+++ Day 1 - Data Types/sol1.py
@@ -5,3 +5,12 @@ Created on Thu Apr 23 17:01:05 2020
 @author: Hasib Iqbal
 """
 
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+j = int(input())
+k = float(input())
+l = input()
+
+print(str(i+j)+'\n'+str(d+k)+'\n'+s+l, end='')
