4699ba3c674c583b5b62d8db1cbd60e45c09ca4b
hasibiqbal207 <hasibiqbal207@gmail.com>
2020-04-23 17:33:22 +0600

    Day3 solved

---
+++ Day 3 - Intro to Conditional Statements/sol3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 17:25:59 2020
+
+@author: Hasib Iqbal
+"""
+
+if __name__ == '__main__':
+    N = int(input())
+    
+    if N % 2 == 1 or N in range(6,21):
+        print('Weird')
+    elif N > 20 or N in range(2,6):
+        print('Not Weird')
